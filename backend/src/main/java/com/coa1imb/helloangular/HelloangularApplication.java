5b55fd0618f2737ecdc95c79978b051d0afac7a9
Fabian <fabiancotic93@gmail.com>
2019-05-06 23:19:12 +0200

    First commit

---
+++ backend/src/main/java/com/coa1imb/helloangular/HelloangularApplication.java
@@ -0,0 +1,13 @@
+package com.coa1imb.helloangular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloangularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloangularApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/coa1imb/helloangular/HelloangularApplicationTests.java
@@ -0,0 +1,16 @@
+package com.coa1imb.helloangular;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HelloangularApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
